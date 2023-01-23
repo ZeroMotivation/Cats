4110a2b5bc6a2607cf4f6e50b76c6fd1a8753065
unknown <home_dv@mail.ru>
2023-01-23 19:59:38 +0300

    Add form validation

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,20 @@ const addBtn = document.querySelector('.add-button');
 const popup = document.querySelector('.add-form');
 const closeBtn = document.querySelector('.close-button');
 const cards = document.querySelector('.cards__container');
-const submitBtn = document.querySelector('.submit-button');
 
 const form = document.forms.cats;
 const inputs = form.elements;
+
+const createCard = (cat) => {
+    return `<div class="cat-card">
+                <img src="${cat.img_link}" alt="" class="card-img">
+                <i class="fa-sharp fa-solid fa-heart ${cat.favourite ? "active" : "inactive"}"></i>
+                <div class="cat-card__content">
+                    <p class="cat-name">${cat.name}</p>
+                    <i class="fa-sharp fa-solid fa-magnifying-glass-plus inactive"></i>
+                </div>
+            </div>`
+} 
 
 addBtn.addEventListener('click', () => popup.classList.toggle('active'));
 
@@ -19,34 +29,26 @@ form.addEventListener('submit', (evt) => {
     let newCat = {};
 
     for(let i = 0; i < inputs.length; i++) {
-        if(inputs[i].name !== 'submit-button') {
+        if(inputs[i].name !== 'submit') {
             newCat[inputs[i].name] = inputs[i].value;
+            inputs[i].value = '';
         }
         if(inputs[i].type === 'checkbox') {
             newCat[inputs[i].name] = inputs[i].checked;
         }
     }
+
     cats.push(newCat);
-    let card = `<div class="cat-card">
-    <img src="${cats[cats.length - 1].img_link}" alt="" class="card-img">
-    <i class="fa-sharp fa-solid fa-heart ${cats[cats.length - 1].favourite ? "active" : "inactive"}"></i>
-    <div class="cat-card__content">
-        <p class="cat-name">${cats[cats.length - 1].name}</p>
-        <i class="fa-sharp fa-solid fa-magnifying-glass-plus inactive"></i>
-    </div>
-    </div>`;
+
+    const lastCat = cats[cats.length - 1];
+
+    let card = createCard(lastCat);
     cards.innerHTML += card;
-    popup.classList.toggle('active');
+
+    popup.classList.remove('active');
 })
 
 cats.forEach((cat) => {
-    let card = `<div class="cat-card">
-                    <img src="${cat.img_link}" alt="" class="card-img">
-                    <i class="fa-sharp fa-solid fa-heart ${cat.favourite ? "active" : "inactive"}"></i>
-                    <div class="cat-card__content">
-                        <p class="cat-name">${cat.name}</p>
-                        <i class="fa-sharp fa-solid fa-magnifying-glass-plus inactive"></i>
-                    </div>
-                </div>`;
+    let card = createCard(cat);
     cards.innerHTML += card;
 })
